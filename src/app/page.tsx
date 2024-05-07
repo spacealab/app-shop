9ac717b6f8cd1575ed8b6d1ad919272cc6bb874c
Ali Alipour <spacealab@gmail.com>
2024-05-07 19:45:18 +0330

    complete Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ export default function Home() {
 
     
       <Layout>
-        <Section>
-          <Banner />
-        </Section>
+      <Section>
+        <Banner title={"Don't miss amazing grocery deals"}
+                subtitle={"Sign up for the daily newsletter"}
+                image={"/assets/images/fresh-apples.png"}
+                bgImage={"/assets/images/banner_bg.png"}/>
+      </Section>
       </Layout>
     </>
   );
--- src/components/common/ui/banner/Banner.tsx
+++ src/components/common/ui/banner/Banner.tsx
@@ -1,25 +1,28 @@
 import { IconBox } from "../icon-box";
 import { ImageView } from "../../image_view";
-import Link from "next/link";
 
 interface Props {
-    
+    title: string;
+    subtitle: string;
+    bgImage: string;
+    image: string;
 }
 
-export function Banner() {
+export function Banner({title, subtitle, bgImage, image}: Props) {
+    // TODO IMPLEMENT FORM
     return (
         <>
-            <div className="grid grid-cols-2 items-center rounded-lg bg-[url('/assets/images/banner_bg.png')] bg-[#FDC040] bg-opacity-20 bg-cover bg-no-repeat">
+            <div style={{backgroundImage: `url('${bgImage}')`}} className={`grid grid-cols-2 items-center rounded-lg bg-[#FDC040] bg-opacity-20 bg-cover bg-no-repeat`}>
                 <div className="pl-[20px] md:pl-[60px]">
-                    <h2 className="text-heading5 sm:text-heading4 lg:text-heading2 xl:text-heading1 2xl:text-display2 text-blue-300 mb-[33px]">Don’t miss amazing grocery deals</h2>
-                    <p className="text-gray-500 mb-[68px] hidden md:inline-block">Sign up for the daily newsletter</p>
+                    <h2 className="text-heading5 sm:text-heading4 lg:text-heading2 xl:text-heading1 2xl:text-display2 text-blue-300 mb-[33px]">{title}</h2>
+                    <p className="text-gray-500 mb-[68px] hidden md:inline-block">{subtitle}</p>
                     <form method="post" action="#" className="hidden lg:flex rounded-[30px] pl-[26px] lg:w-[442px] w-full bg-white items-center">
                     <IconBox icon="icon-paper-plane" size={24}/>
                     <input type="email" name="email" placeholder="Your email address" className="pl-2 font-lato text-xsmall grow focus:outline-none bg-transparent" />
                     <input type="submit" value="subscribe" className="text-white hover:bg-green-300 bg-green-200 rounded-[30px] px-[18px] py-2 sm:px-[37px] sm:py-[15px] cursor-pointer text-heading6" />
                     </form>
                 </div>
-                <ImageView src="/assets/images/fresh-apples.png" className="m-w-[50%]" alt="banner" width={717} height={538}/>
+                <ImageView src={image} className="m-w-[50%]" alt="banner" width={717} height={538}/>
             </div>
             <form name="subscription" method="post" action="#" className="flex lg:hidden gap-2.5 items-center justify-between rounded-[30px] mx-auto max-w-[550px] bg-[#F5F5F5] pl-4 sm:pl-5 md:pl-6 mt-4 sm:mt-5 md:mt-6">
                 <IconBox icon="icon-paper-plane" size={24} link="#"/>
