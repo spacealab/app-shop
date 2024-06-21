f5ce9e99e639b68c427f2559b8f2539afbf1428d
Ali Alipour <spacealab@gmail.com>
2024-06-21 21:08:27 +0330

    form register

--- src/components/common/auth/RegisterModal.tsx
+++ src/components/common/auth/RegisterModal.tsx
@@ -1,13 +1,42 @@
 import { Modal } from "@/components";
+import { useForm } from "react-hook-form";
 
 interface Props {
     onClose: () => void
 }
 
-export function RegisterModal({onClose}: Props) {
+interface FormData {
+    username: string;
+    email: string;
+    passwords: string;
+}
+
+export function RegisterModal({ onClose }: Props) {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onSubmit = (data: FormData): void => {
+    }
+
     return (
         <Modal title="register" closeModal={onClose}>
-            <form></form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="username">username:</label>
+                    <input id="username" type="text" placeholder="enter your username" {...register('username', { required: true })} />
+                </div>
+
+                <div>
+                    <label htmlFor="email">email:</label>
+                    <input id="email" type="email" {...register('email', { required: "enter your email please" })} />
+                </div>
+
+                <div>
+                    <label htmlFor="passwords">passwords:</label>
+                    <input id="passwords" type="passwords" {...register('passwords', { required: "enter your passwords please", minLength:{value: 3, message: " min 3 character"} })}/>
+                </div>
+            </form>
+
+            <button className="mt-2 bg-[#efefef]">submit</button>
         </Modal>
     );
-};
+}
