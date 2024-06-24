881742c7c6c8714f9389504a5f911125cc7595bc
Ali Alipour <spacealab@gmail.com>
2024-06-24 23:16:24 +0330

    function shop card context

--- src/store/BasketContext.tsx
+++ src/store/BasketContext.tsx
@@ -1,5 +1,8 @@
 import { ReactNode, createContext, useState } from "react";
 
+import { EntityType } from "@/types";
+import { ProductType } from "@/types/api/Product";
+
 interface Props {
   children: ReactNode;
 }
@@ -8,48 +11,83 @@ interface ProductItem {
     productId: number;
     title: string;
     price: number;
-    img: string;
+    img?: string;
     quantity: number;
   }
 
   export const BasketContext = createContext<{
     basketItems: Array<ProductItem>,
-    addItem: (product: ProductItem) => void,
+    addItem: (product: EntityType<ProductType>) => void,
     increaseItem: (productId: number) => void,
     decreaseItem: (productId: number) => void,
     deleteItem: (productId: number) => void
   }>({
     basketItems: [],
-    addItem: (product: ProductItem): void => {},
+    addItem: (product: EntityType<ProductType>): void => {},
     increaseItem: (productId: number): void => {},
     decreaseItem: (productId: number): void => {},
     deleteItem: (productId: number): void => {}
-  });  
+  });
+  
 
 export const BasketContextProvider = (props: Props) => {
   const [basketItems, setBasketItems] = useState<Array<ProductItem>>([]);
 
-  const addItemHandler = (product: ProductItem): void => {
-
-  }
+  const addItemHandler = (product: EntityType<ProductType>) => {
+    const newProduct: ProductItem = {
+      productId: product.id,
+      title: product.attributes.title,
+      img: product.attributes.thumbnail?.data?.attributes.url,
+      price: product.attributes.price,
+      quantity: 1
+    };
+  
+    setBasketItems((prevState) => [
+      ...prevState,
+      newProduct
+    ]);
+  };
+  
   
   const increaseItemHandler = (productId: number): void => {
+    const newBasket = basketItems.map((item) => {
+      if (item.productId === productId) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
   
-  }
+    setBasketItems(newBasket);
+  };
+  
   
   const decreaseItemHandler = (productId: number): void => {
+    const currentProduct = basketItems.find((item) => item.productId === productId);
+    
+    if (currentProduct && currentProduct.quantity === 1) {
+      deleteItemHandler(productId)
+    } else {
+      const newBasket = basketItems.map((item) => {
+        if (item.productId === productId) {
+          return { ...item, quantity: item.quantity - 1 };
+        }
+        return item;
+      });
+      setBasketItems(newBasket);
+    }
+  };
   
-  }
   
   const deleteItemHandler = (productId: number): void => {
-  
+    const newBasket = basketItems.filter((item) => item.productId !== productId);
+    setBasketItems(newBasket);
   }
   
 
   return (
     <BasketContext.Provider
       value={{
-        basketItem: basketItems,
+        basketItems: basketItems,
         addItem: addItemHandler,
         increaseItem: increaseItemHandler,
         decreaseItem: decreaseItemHandler,
