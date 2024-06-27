2a05fa45ffb5d36bb5a56c2396f7e8ce8bff0985
Ali Alipour <spacealab@gmail.com>
2024-06-27 11:33:38 +0330

    connect shop card

--- src/api/Basket.ts
+++ src/api/Basket.ts
@@ -12,10 +12,41 @@ export interface UpdateBasketData {
 }
 
 export async function basketApiCall(): Promise<ApiResponseSingleType<BasketType>> {
+
+    const token = window.localStorage.getItem('token');
+    const uuid = window.localStorage.getItem('uuid');
+    
+    if (!token && !uuid) {
+        const response: ApiResponseSingleType<BasketType> = await apiClient.post('/my-basket');
+        window.localStorage.setItem('uuid', response.data.attributes.uuid);
+        return response;
+    }    
+
+    if (uuid) {
+        return await apiClient.get('/my-basket', {
+            params: {
+                uuid: uuid
+            }
+        });
+    }    
+
     return await apiClient.get('/my-basket');
 }
 
 export async function updateBasketApiCall(data: UpdateBasketData){
+
+    const uuid = window.localStorage.getItem('uuid');
+
+    if (uuid) {
+        return await apiClient.put('/my-basket', {
+            data: data
+        }, {
+            params: {
+                uuid: uuid
+            }
+        });
+    }
+    
     return await apiClient.put('/my-basket', {
         data: data
     });
